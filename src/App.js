0d4a1f089152b23c601b9c4513f35a3825ecb7e7
Chris K <Chris-SourceCode@outlook.com>
2021-09-12 17:46:23 +0100

    feat: Move sample data to SAMPLE_POSTS

--- src/App.js
+++ src/App.js
@@ -6,37 +6,13 @@ import NewPost from './components/NewPost';
 import PostPage from './components/PostPage';
 import About from './components/About';
 import Missing from './components/Missing';
+import SAMPLE_POSTS from './data/SAMPLE_POSTS';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'My First Post',
-      datetime: 'September 12, 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-    },
-    {
-      id: 2,
-      title: 'My 2nd Post',
-      datetime: 'September 12, 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-    },
-    {
-      id: 3,
-      title: 'My 3rd Post',
-      datetime: 'September 01, 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-    },
-    {
-      id: 4,
-      title: 'My Fourth Post',
-      datetime: 'September 12, 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-    },
-  ]);
+  const [posts, setPosts] = useState(SAMPLE_POSTS);
 
   const [search, setSearch] = useState('');
   const [postTitle, setPostTitle] = useState('');
