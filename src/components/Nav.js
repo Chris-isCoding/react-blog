eaf6aadf1418890203784256a1aeac4967155b19
Chris K <Chris-SourceCode@outlook.com>
2021-09-12 14:33:55 +0100

    feat: Expand Nav component
     - Add search form and React Router Links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,29 @@
-const Nav = () => {
+import { Link } from 'react-router-dom';
+
+const Nav = ({ search, setSearch }) => {
   return (
-    <nav>
-      <h2>Nav</h2>
+    <nav className='Nav'>
+      <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor='search'>Search Post</label>
+        <input
+          id='search'
+          type='text'
+          placeholder='Search Posts'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/post'>Post</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
