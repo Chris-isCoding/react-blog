1c359c26cc5ac309a9876f927fb72645e563c6c6
Chris K <Chris-SourceCode@outlook.com>
2021-09-12 14:42:59 +0100

    feat: Pass props to components & add starter data

--- src/App.js
+++ src/App.js
@@ -10,15 +10,41 @@ import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'My First Post',
+      datetime: 'September 12, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 2,
+      title: 'My 2nd Post',
+      datetime: 'September 12, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 3,
+      title: 'My 3rd Post',
+      datetime: 'September 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 4,
+      title: 'My Fourth Post',
+      datetime: 'September 12, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+  ]);
   const [search, setSearch] = useState('');
 
   return (
     <div className='App'>
       <Header title='React JS Blog' />
-      <Nav />
+      <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path='/'>
-          <Home />
+          <Home posts={posts} />
         </Route>
         <Route exact path='/post'>
           <NewPost />
