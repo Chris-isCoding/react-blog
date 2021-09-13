00570eec58d7f0cb5760ce50f078313b7b995199
Chris K <Chris-SourceCode@outlook.com>
2021-09-13 08:28:43 +0100

    feat: Expand About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,17 @@
 const About = () => {
   return (
-    <main>
+    <main className='About'>
       <h2>About</h2>
+      <p style={{ marginTop: '1rem' }}>
+        This blog app is a project to practice and learn React.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Accusamus pariatur suscipit impedit? Aperiam placeat dicta
+        consequatur vero reiciendis eius tempora illo aliquid,
+        temporibus alias ullam iure dignissimos. Doloribus, facilis et?
+      </p>
     </main>
   );
 };
