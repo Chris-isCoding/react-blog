0fb3c1fb4e9f637632a308703dc64c8f10dc69bf
Chris K <Chris-SourceCode@outlook.com>
2021-09-14 22:45:54 +0100

    feat: Fetch posts with Axios

--- src/App.js
+++ src/App.js
@@ -6,19 +6,46 @@ import NewPost from './components/NewPost';
 import PostPage from './components/PostPage';
 import About from './components/About';
 import Missing from './components/Missing';
-import SAMPLE_POSTS from './data/SAMPLE_POSTS';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import api from './api/posts.js';
 
 function App() {
-  const [posts, setPosts] = useState(SAMPLE_POSTS);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState('');
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
 
   const history = useHistory();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error:', error.message);
+        }
+        console.log(error.config);
+      }
+    };
+    fetchPosts();
+  }, []);
 
   useEffect(() => {
     const filteredPosts = posts.filter(
