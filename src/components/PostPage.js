8fc5c75ee7af1ae64374ec01b8cf6d5f7603e4d2
Chris K <Chris-SourceCode@outlook.com>
2021-09-15 16:00:16 +0100

    feat: Add edit button to PostPage component

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -12,6 +12,9 @@ const PostPage = ({ posts, handleDelete }) => {
             <h2>{post.title}</h2>
             <p className='postDate'>{post.datetime}</p>
             <p className='postBody'>{post.body}</p>
+            <Link to={`/edit/${post.id}`}>
+              <button className='editButton'>Edit Post</button>
+            </Link>
             <button onClick={() => handleDelete(post.id)}>
               Delete Post
             </button>
