ca2f910fa47fda0561bf34fd4b113554f871a53a
Chris K <Chris-SourceCode@outlook.com>
2021-09-17 21:47:48 +0100

    refactor: Clean-up useWindowSize hoop
     - remove console.log from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa';
 
 const Header = ({ title, width }) => {
-  console.log(width);
   return (
     <header className='Header'>
       <h1>{title}</h1>
--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -18,12 +18,7 @@ const useWindowSize = () => {
 
     window.addEventListener('resize', handleResize);
 
-    const cleanUp = () => {
-      console.log('runs if a useEffect dependency changes');
-      window.removeEventListener('resize', handleResize);
-    };
-
-    return cleanUp;
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return windowSize;
