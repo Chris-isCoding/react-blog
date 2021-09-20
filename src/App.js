97cd90f9711be4e1c63d02928a534c0f2d885b01
Chris K <Chris-SourceCode@outlook.com>
2021-09-20 21:19:45 +0100

    refactor: Clean-up App component

--- src/App.js
+++ src/App.js
@@ -3,79 +3,15 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import NewPost from './components/NewPost';
+import EditPost from './components/EditPost';
 import PostPage from './components/PostPage';
 import About from './components/About';
 import Missing from './components/Missing';
-import EditPost from './components/EditPost';
-import { Route, Switch, useHistory } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { format } from 'date-fns';
-import api from './api/posts.js';
-import useAxiosFetch from './hooks/useAxiosFetch';
+
+import { Route, Switch } from 'react-router-dom';
 import { DataProvider } from './context/DataContext';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [search, setSearch] = useState('');
-  const [searchResults, setSearchResults] = useState('');
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
-  const [editTitle, setEditTitle] = useState('');
-  const [editBody, setEditBody] = useState('');
-
-  const history = useHistory();
-  const { data, fetchError, isLoading } = useAxiosFetch(
-    'http://localhost:3500/posts'
-  );
-
-  useEffect(() => {
-    setPosts(data);
-  }, [data]);
-
-  useEffect(() => {
-    const filteredPosts = posts.filter(
-      (post) =>
-        post.body.toLowerCase().includes(search.toLowerCase()) ||
-        post.title.toLowerCase().includes(search.toLowerCase())
-    );
-    setSearchResults(filteredPosts.reverse());
-  }, [posts, search]);
-
-  const handleEdit = async (id) => {
-    const datetime = format(new Date(), 'MMMM dd, yyyy HH:mm:ss');
-    const editedPost = {
-      id,
-      datetime: datetime,
-      title: editTitle,
-      body: editBody,
-    };
-
-    try {
-      const response = await api.put(`/posts/${id}`, editedPost);
-      setPosts(
-        posts.map((post) =>
-          post.id === id ? { ...response.data } : post
-        )
-      );
-      setEditTitle('');
-      setEditBody('');
-      history.push('/');
-    } catch (error) {
-      console.log(`Something went wrong: ${error.message}`);
-    }
-  };
-
-  const handleDelete = async (id) => {
-    try {
-      await api.delete(`/posts/${id}`);
-      const postsList = posts.filter((post) => post.id !== id);
-      setPosts(postsList);
-      history.push('/');
-    } catch (error) {
-      console.log(`Something went wrong: ${error.message}`);
-    }
-  };
-
   return (
     <div className='App'>
       <DataProvider>
