cff89bfbdb73ade68a02fb254f3b334c1a7f0322
Chris K <Chris-SourceCode@outlook.com>
2021-09-23 23:26:20 +0100

    feat: Add StoreActions

--- src/App.js
+++ src/App.js
@@ -8,24 +8,35 @@ import PostPage from './components/PostPage';
 import About from './components/About';
 import Missing from './components/Missing';
 
+import { useEffect } from 'react';
+import { useStoreActions } from 'easy-peasy';
+import useAxiosFetch from './hooks/useAxiosFetch';
 import { Route, Switch } from 'react-router-dom';
-import { DataProvider } from './context/DataContext';
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts);
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    'http://localhost:3500/posts'
+  );
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data, setPosts]);
+
   return (
     <div className='App'>
       <Header title='React JS Blog' />
-      <DataProvider>
-        <Nav />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/post' component={NewPost} />
-          <Route path='/edit/:id' component={EditPost} />
-          <Route path='/post/:id' component={PostPage} />
-          <Route path='/about' component={About} />
-          <Route path='*' component={Missing} />
-        </Switch>
-      </DataProvider>
+      <Nav />
+      <Switch>
+        <Route exact path='/'>
+          <Home isLoading={isLoading} fetchError={fetchError} />
+        </Route>
+        <Route exact path='/post' component={NewPost} />
+        <Route path='/edit/:id' component={EditPost} />
+        <Route path='/post/:id' component={PostPage} />
+        <Route path='/about' component={About} />
+        <Route path='*' component={Missing} />
+      </Switch>
       <Footer />
     </div>
   );
