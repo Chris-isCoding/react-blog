aff1d0988425a7ae29d51be32e8867fbec6dfc7d
Chris K <Chris-SourceCode@outlook.com>
2021-09-30 23:28:52 +0100

    feat: Implement Easy Peasy Redux in EditPost

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,16 +1,20 @@
-import { useState, useEffect, useContext } from 'react';
+import { useEffect } from 'react';
 import { useParams, Link, useHistory } from 'react-router-dom';
-import api from '../api/posts';
-import { DataContext } from '../context/DataContext';
 import { format } from 'date-fns';
+import { useStoreState, useStoreActions } from 'easy-peasy';
 
 const EditPost = () => {
-  const [editTitle, setEditTitle] = useState('');
-  const [editBody, setEditBody] = useState('');
-  const { posts, setPosts } = useContext(DataContext);
   const history = useHistory();
   const { id } = useParams();
-  const post = posts.find((post) => post.id.toString() === id);
+
+  const editTitle = useStoreState((state) => state.editTitle);
+  const editBody = useStoreState((state) => state.editBody);
+  const editPost = useStoreActions((actions) => actions.editPost);
+  const setEditTitle = useStoreActions((actions) => actions.setEditTitle);
+  const setEditBody = useStoreActions((actions) => actions.setEditBody);
+
+  const getPostById = useStoreState((state) => state.getPostById);
+  const post = getPostById(id);
 
   const handleEdit = async (id) => {
     const datetime = format(new Date(), 'MMMM dd, yyyy HH:mm:ss');
@@ -20,21 +24,8 @@ const EditPost = () => {
       body: editBody,
       datetime: datetime,
     };
-
-    try {
-      const response = await api.patch(`/posts/${id}`, editedPost);
-
-      setPosts(
-        posts.map((post) =>
-          post.id === id ? { ...response.data } : post
-        )
-      );
-      setEditTitle('');
-      setEditBody('');
-      history.push('/');
-    } catch (error) {
-      console.log(`Something went wrong: ${error.message}`);
-    }
+    editPost(editedPost);
+    history.push(`/post/${id}`);
   };
 
   useEffect(() => {
@@ -47,10 +38,7 @@ const EditPost = () => {
   return post ? (
     <main className='NewPost'>
       <h2>Edit Post</h2>
-      <form
-        className='newPostForm'
-        onSubmit={(e) => e.preventDefault()}
-      >
+      <form className='newPostForm' onSubmit={(e) => e.preventDefault()}>
         <label htmlFor='postTitle'>Title:</label>
         <input
           id='postTitle'
