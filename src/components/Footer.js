a52670fbcc9ae7cd1d92e24439f70e4c6dd675d0
Chris K <Chris-SourceCode@outlook.com>
2021-10-07 22:56:38 +0100

    feat: Display number of posts in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
+import { useStoreState } from 'easy-peasy';
+
 const Footer = () => {
-  const today = new Date();
+  const postCount = useStoreState((state) => state.postCount);
   return (
     <footer className='Footer'>
-      <p>Copyright &copy; {today.getFullYear()}</p>
+      <p>{postCount} Blog Posts</p>
     </footer>
   );
 };
